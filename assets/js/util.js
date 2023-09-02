a07656e99c391b14f0f8dccc62723d76941028ca
FabioHCNobre <fabiohcnobre@hotmail.com>
2023-09-02 17:32:06 -0300

    Versão 02
    Versão 02

--- assets/js/util.js
+++ assets/js/util.js
@@ -432,8 +432,7 @@
 
 		// Events.
 			$this
-				.on('submit', function () {
-								
+				.on('submit', function() {
 
 					$this.find('input[type=text],input[type=password],textarea')
 						.each(function(event) {
